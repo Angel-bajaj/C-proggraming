bc554a4a7f71f899fb4b7d956b1693e66f7c3559
Angel <angelbajaj919@gmail.com>
2025-09-17 13:20:13 +0530

    Day-3

--- Question6.c
+++ Question6.c
@@ -1,7 +1,6 @@
 //Q6: Write a program to swap two numbers using a third variable.
 
 #include <stdio.h>
-
 int main()
 { 
     int variable1, variable2, variable3;
@@ -12,6 +11,7 @@ int main()
     variable3 = variable1;
     variable1 = variable2;
     variable2 = variable3;
+    
     printf("HAHA I swapped your variable1: %d\n", variable1);
     printf("And also your variable2: %d\n", variable2);
     return 0;
